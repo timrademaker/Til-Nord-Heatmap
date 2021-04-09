7791da8c37f064c2eb9bfd8ad74700fa39d37bca
timrademaker <10331650+timrademaker@users.noreply.github.com>
2021-04-09 17:12:21 +0200

    Added settings window

---
+++ HeatmapWrapper/SettingsWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Forms;
+
+namespace HeatmapWrapper
+{
+    /// <summary>
+    /// Interaction logic for SettingsWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow : Window
+    {
+        private OpenFileDialog BackgroundImageFileDialog;
+
+        public SettingsWindow()
+        {
+            InitializeComponent();
+
+            BackgroundImageFileDialog = new OpenFileDialog();
+            BackgroundImageFileDialog.RestoreDirectory = true;
+            BackgroundImageFileDialog.Title = "Select Heatmap Background Image";
+            BackgroundImageFileDialog.Filter = "Image files (*.png;*.jpg;*.jpeg)|*.png;*.jpg;*.jpeg|All files (*.*)|*.*";
+            BackgroundImageFileDialog.CheckPathExists = true;
+            BackgroundImageFileDialog.CheckFileExists = true;
+        }
+
+        private void BackgroundImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Open file browser and update text in block
+            if(BackgroundImageFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                tbBackgroundImage.Text = BackgroundImageFileDialog.FileName;
+            }
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void ConfirmButton_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Save settings
+            Close();
+        }
+    }
+}
